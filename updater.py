5459a2e9324f011e34fbd7bb0b3c7f23a2c5af59
Juan Latorre <juanlatorre@protonmail.com>
2017-04-18 21:22:07 -0300

    Added a Folder Chooser to change the default App Folder

--- updater.py
+++ updater.py
@@ -1,24 +1,42 @@
-import glob, os, yaml
+import glob, os
 from appJar import gui
 
+#Setting the default AppFolder:
+#folder = "/home/unnamed_query/Apps"
+
 #We read the user settings before starting the app.
-with open("config.yaml", "r" as stream:
-        try:
-            print(yaml.load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
+def setAppFolder():
+    global folder
+    try:
+        prefs_file = open("prefs.txt")
+    except OSError as err:
+        # Esto queda pendiente de revisión, hasta ahora funciona bien si es que el archivo ha sido creado previamente.
+        print("There are no prefs, attempting to create it...")
+        app.infoBox("Choose App folder", "There is no App Folder set, please select the folder where you AppImages are.")
+        folder = app.directoryBox(title="Select you App Folder", dirName="~/")
+        new_prefs_file = "prefs.txt"
+        write_prefs = open(new_prefs_file, 'w')
+        write_prefs.write(folder)
+    else:
+        folder = prefs_file.read()
+        folder = folder.split('\n')[0]
+        os.chdir(folder)
+
+#We open a dialog to choose the folder, then we call setAppFolder.
+def changeAppFolder():
+    print("changed")
 
 # Function to handle the election of a Radio Check
 def choose(rb):
-    selected[0] = app.getRadioButton(rb)
+    global selected
+    selected = app.getRadioButton(rb)
 
 def press(name):
     if (name=="Change App Folder"):
-        print("Folder changed")
+        changeAppFolder()
 
     if (name=="Update"):
         print("Beginning updating process of",selected[0])
-        app.setStatusbar(updating, 0)
 
     if (name=="Exit"):
         app.stop()
@@ -27,11 +45,11 @@ def press(name):
 app = gui()
 app.setTitle("Application Updater")
 app.setResizable(False)
+setAppFolder()
 
 #Adding a Label with Text
 app.addLabel("welcome", "This little script allow you to update your AppImages in ~/Apps\nSelect the Application that you want to update:")
-#Focus on Apps Folder
-os.chdir("/home/unnamed_query/Apps")
+
 #For every App in the folder, fill a list of Radio Button
 for file in glob.glob("*.AppImage"):
     #appList.append(file)
